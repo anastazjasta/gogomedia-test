356b0705f74bd607e2535fb2b24bf96c47dd9f8a
Anastazjasta <anastazja.sta@gmail.com>
2021-01-12 16:45:51 +0100

    feat: slider

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,13 @@
 import '../scss/main.scss';
 
-console.log('HELLO 🚀')
+window.addEventListener('load', function(){
+    new Glider(document.querySelector('.glider'), {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        draggable: true,
+        arrows: {
+          prev: '.glider-prev',
+          next: '.glider-next'
+        }
+    })
+  })
